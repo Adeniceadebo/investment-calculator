3e5302badcd23b2c2ec83ab373fe25ea26384495
Adebowalem Adedayo <adeniceadebo@gmail.com>
2025-02-09 22:06:30 +0100

    changes

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,16 @@
+export default function UserInput() {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input type="text" />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input type="text" />
+        </p>
+      </div>
+    </section>
+  );
+}
